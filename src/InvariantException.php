d380953ca1a96b372fde42a9161a7600e6251a9d
Max Zheltikov <maxcruer@gmail.com>
2021-07-13 12:20:23 +0300

    added a bunch of exceptions

---
+++ src/InvariantException.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Zheltikov\Exceptions;
+
+use Exception;
+
+/**
+ * Class InvariantException
+ * @package Zheltikov\Exceptions
+ */
+class InvariantException extends Exception
+{
+}
---
+++ src/TypeAssertionException.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Zheltikov\Exceptions;
+
+use Exception;
+
+/**
+ * Class TypeAssertionException
+ * @package Zheltikov\Exceptions
+ */
+final class TypeAssertionException extends Exception
+{
+}
